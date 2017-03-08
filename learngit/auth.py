6608e69eb7f563dec4828e6d706bfe7170eadeb5
kimjaejoong <577955697@qq.com>
2017-03-08 16:49:58 +0800

    add auth.py

---
+++ learngit/auth.py
@@ -0,0 +1,20 @@
+#——conding:utf8——
+
+from oslo_config import cfg
+from oslo_log import log as logging
+from oslo_log import versionutils
+from oslo_middleware import request_id
+from oslo_serialization import jsonutils
+import webob.dec
+import webob.exc
+
+from nova import context
+
+
+class ImageClass(){
+        def _init__(self):
+            self.discard = discard
+        def get_item(self):
+            
+        
+        }
